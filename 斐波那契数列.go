feb93b0634cab87200cd0a586cd3c5aa3b148d03
laozaoshuijiao <yixianshiki@gmail.com>
2019-06-08 15:19:01 +0800

    二次

--- 斐波那契数列.go
+++ 斐波那契数列.go
@@ -2,4 +2,17 @@ package main
 
 import "fmt"
 
-func fibona
+func fibonacci(n int)int{
+	if n < 2{
+		return n
+	}
+	return fibonacci(n - 2)+fibonacci(n-1)
+}
+
+
+func main(){
+
+	for i := 0;i < 10; i++{
+		fmt.Printf("%d\t",fibonacci(i))
+	}
+}
