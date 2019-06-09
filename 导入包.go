35000bc67b4ff63ec839e49e99f5da7887ee4cf1
laozaoshuijiao <yixianshiki@gmail.com>
2019-06-09 18:18:21 +0800

    导入包

---
+++ 导入包.go
@@ -0,0 +1,35 @@
+package main
+/*
+import (
+	"fmt" //导入包必须使用
+	"os"
+)
+
+func main() {
+	fmt.Println("hello world!")
+	fmt.Println("os Arge",os.Args)
+
+}
+
+ */
+/*
+import . "fmt" //调用函数 ，无需包名
+
+func main(){
+	Println("hello world!")
+}
+
+ */
+/*
+import io "fmt"
+
+func main(){
+	io.Println("hello woeld")
+}
+
+ */
+
+//忽略此包
+
+
+
